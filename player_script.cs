31fb0cbcefa8ba29f95d8bef6834fa173b04e950
valaosvibecoder-dotcom <kgvalasiadis@gmail.com>
2025-11-27 11:01:30 +0200

    Player code commit

---
+++ player_script.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class player_script : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
+        {
+            rb.linearVelocity=Vector2.right*3f;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
+        {
+            rb.linearVelocity=Vector2.left*3f;
+        }
+    }
+
+}
